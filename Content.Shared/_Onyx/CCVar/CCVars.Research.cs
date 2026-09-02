14f8012b3142cde6ddbe4975ccd8c783c5d2a806
Neyran <maxnezerplay@gmail.com>
2026-09-02 20:45:30 +0300

    Update CCVars.Research.cs

--- Content.Shared/_Onyx/CCVar/CCVars.Research.cs
+++ Content.Shared/_Onyx/CCVar/CCVars.Research.cs
@@ -5,5 +5,5 @@ namespace Content.Shared.CCVar;
 public sealed partial class CCVars
 {
     public static readonly CVarDef<int> ArtifactNodeExperimentalReward =
-        CVarDef.Create("research.artifact_node_experimental_reward", 110, CVar.SERVER | CVar.REPLICATED);
+        CVarDef.Create("research.artifact_node_experimental_reward", 180, CVar.SERVER | CVar.REPLICATED);
 }
