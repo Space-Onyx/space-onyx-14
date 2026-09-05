e90ac0eed4b9a866608ae07ecfedd0e0165ea75b
Neyran <maxnezerplay@gmail.com>
2026-09-05 21:25:16 +0300

    [PATCH] Усиление толчков в боевом режиме.

--- Content.Shared/_Onyx/Shoving/ShovingComponent.cs
+++ Content.Shared/_Onyx/Shoving/ShovingComponent.cs
@@ -3,7 +3,7 @@ namespace Content.Shared.Weapons.Melee;
 [RegisterComponent]
 public sealed partial class ShovingComponent : Component
 {
-    public const float DefaultStaminaDamage = 10f;
+    public const float DefaultStaminaDamage = 14f;
 
     [DataField]
     public float StaminaDamage = DefaultStaminaDamage;
