49270199def5c004731d0202328d2c20178cbaa8
Neyran <maxnezerplay@gmail.com>
2026-09-07 00:35:48 +0300

    Update GameTicker.UserActions.cs

--- Content.Server/_Onyx/UserActions/GameTicker.UserActions.cs
+++ Content.Server/_Onyx/UserActions/GameTicker.UserActions.cs
@@ -13,22 +13,11 @@ public sealed partial class GameTicker
         if (preset == null)
             return;
 
-        var stationNames = new StringBuilder();
-        var query = EntityQueryEnumerator<StationJobsComponent, StationSpawningComponent, MetaDataComponent>();
-
-        while (query.MoveNext(out _, out _, out var meta))
-        {
-            if (stationNames.Length > 0)
-                stationNames.Append('\n');
-
-            stationNames.Append(meta.EntityName);
-        }
-
         if (stationNames.Length == 0)
             stationNames.Append(_gameMapManager.GetSelectedMap()?.MapName ?? Loc.GetString("game-ticker-no-map-selected"));
 
         RaiseNetworkEvent(new TickerInGameInfoEvent(
-                stationNames.ToString(),
+                _gameMapManager.GetSelectedMap()?.MapName ?? Loc.GetString("game-ticker-no-map-selected"),
                 RoundId,
                 Decoy == null ? Loc.GetString(preset.ModeTitle) : Loc.GetString(Decoy.ModeTitle),
                 _playerManager.PlayerCount),
