5171b608bb3d76dd9d725dd86583915a28bf4c43
Neyran <maxnezerplay@gmail.com>
2026-09-19 19:58:10 +0300

    [ADD] Эффект речи.

--- Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
+++ Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
@@ -19,7 +19,6 @@ using Robust.Shared.ContentPack;
 using Robust.Shared.Enums;
 using Robust.Shared.Prototypes;
 using Direction = Robust.Shared.Maths.Direction;
-using Content.Shared._Onyx.CCVar;
 using Content.Shared._Onyx.SpeechBarks;
 
 namespace Content.Client.Lobby.UI
@@ -225,7 +224,7 @@ namespace Content.Client.Lobby.UI
             // <Onyx-Barks>
             #region Voice
 
-            if (configurationManager.GetCVar(ADTCCVars.BarksEnabled))
+            if (configurationManager.GetCVar(CCVars.BarksEnabled))
             {
                 BarksContainer.Visible = true;
                 InitializeBarks();
--- Content.Shared/_Onyx/CCVar/CCVars.Barks.cs
+++ Content.Shared/_Onyx/CCVar/CCVars.Barks.cs
@@ -1,15 +1,29 @@
 using Robust.Shared.Configuration;
 
-namespace Content.Shared._Onyx.CCVar;
+namespace Content.Shared.CCVar;
 
-[CVarDefs]
-public sealed class ADTCCVars
+public sealed partial class CCVars
 {
-    /*
-    * Barks
-    */
     public static readonly CVarDef<bool> BarksEnabled =
         CVarDef.Create("barks.enabled", true, CVar.SERVER | CVar.REPLICATED | CVar.ARCHIVE);
+
+    public static readonly CVarDef<bool> SpeechBubbleRevealEnabled =
+        CVarDef.Create("barks.speech_bubble_reveal_enabled", true, CVar.SERVER | CVar.REPLICATED | CVar.ARCHIVE);
+
+    public static readonly CVarDef<float> BarksMaxSpeechDuration =
+        CVarDef.Create("barks.max_speech_duration", 4f, CVar.SERVER | CVar.REPLICATED | CVar.ARCHIVE);
+
+    public static readonly CVarDef<float> BarksShoutSpeedMultiplier =
+        CVarDef.Create("barks.shout_speed_multiplier", 0.7f, CVar.SERVER | CVar.REPLICATED | CVar.ARCHIVE);
+
+    public static readonly CVarDef<float> BarksCommaPause =
+        CVarDef.Create("barks.comma_pause", 0.15f, CVar.SERVER | CVar.REPLICATED | CVar.ARCHIVE);
+
+    public static readonly CVarDef<float> BarksSentencePause =
+        CVarDef.Create("barks.sentence_pause", 0.25f, CVar.SERVER | CVar.REPLICATED | CVar.ARCHIVE);
+
+    public static readonly CVarDef<float> BarksEllipsisPause =
+        CVarDef.Create("barks.ellipsis_pause", 0.55f, CVar.SERVER | CVar.REPLICATED | CVar.ARCHIVE);
 
     public static readonly CVarDef<float> BarksMaxPitch =
         CVarDef.Create("barks.max_pitch", 1.5f, CVar.SERVER | CVar.REPLICATED | CVar.ARCHIVE);
